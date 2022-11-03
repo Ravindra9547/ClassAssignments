34571e96ed545fa900bb7621e8391e0eab45e7e2
Ravindra9547 <117341138+Ravindra9547@users.noreply.github.com>
2022-11-03 14:29:02 +0530

    Add files via upload

---
+++ DoubleValue.java
@@ -0,0 +1,13 @@
+//Write a program to print two double variable.
+
+package basicprogram;
+
+public class DoubleValue {
+public static void main(String []arg)
+{
+	double a=2345.98,b=4567.678;
+	System.out.println("The 1st double value is:" +a);
+	System.out.println("The 2nd double value is:" +b);
+	
+}
+}
