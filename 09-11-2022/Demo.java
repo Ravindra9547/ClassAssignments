1f130fb34ab29f92b6fc91d5a957248a2b1b85d4
Ravindra Kumar Saw <117341138+Ravindra9547@users.noreply.github.com>
2022-11-11 22:03:21 +0530

    Add files via upload

---
+++ 09-11-2022/Demo.java
@@ -0,0 +1,17 @@
+//using method
+package oopsconcept;
+class Stud
+{
+	int id;
+	String name,address,course;
+	float fees;
+}
+public class Demo {
+public static void main(String []args)	
+{
+	Stud ravindra=new Stud();
+	System.out.println(ravindra.id+" "+ravindra.name+" "+ravindra.fees);
+	
+}
+
+}
