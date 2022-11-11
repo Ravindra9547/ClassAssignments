b67d0f126844c9a58429ab0f2e7a9cb152c48eeb
Ravindra Kumar Saw <117341138+Ravindra9547@users.noreply.github.com>
2022-11-11 20:08:15 +0530

    Add files via upload

---
+++ 04-11-2022/CountingDigits.java
@@ -0,0 +1,24 @@
+//Write a program to count number of digits in a number.
+//using for loop
+
+package basicprogram;
+
+import java.util.Scanner;
+
+public class CountingDigits {
+public static void main(String []args)
+{
+	Scanner sc = new Scanner(System.in);
+    int n,count=0;
+    System.out.println("Enter a number to count:");  // input number from user
+    n =sc.nextInt();
+    
+    for(;n!=0;)    //run loop till n is greater than 0
+    {
+       count++;
+       n=n/10;     // remove last digit of 'n'
+    }
+    System.out.println("The Total number of Digits = " +count);  // display the total digit/output 
+ }
+
+}
